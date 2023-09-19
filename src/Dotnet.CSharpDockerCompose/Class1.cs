7960b6fca1025eb231e815ed88dd67ce9d9f9d23
William Rezende | Stone <william.rezende@stone.com.br>
2023-09-19 18:56:37 -0300

    First commit

---
+++ src/Dotnet.CSharpDockerCompose/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Dotnet.CSharpDockerCompose
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ test/Dotnet.CSharpDockerCompose.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Dotnet.CSharpDockerCompose.Unit.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
